f93c1b6bb28c580732fb47be6514342ea13bdd13
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-09-16 21:05:35 +0800

    feat: add methodology section with new images for enhanced content presentation

--- src/routes/study/+page.svelte
+++ src/routes/study/+page.svelte
@@ -4,6 +4,11 @@
 	import evaluationIMG from '$lib/assets/flaticon/evaluation.png';
 	import sortingIMG from '$lib/assets/flaticon/sorting.png';
 	import network from '$lib/assets/flaticon/global-network.png';
+
+	import analysisIMG from '$lib/assets/flaticon/analysis.png';
+	import databaseIMG from '$lib/assets/flaticon/data-server.png';
+	import cogsIMG from '$lib/assets/flaticon/cogs.png';
+	import layerIMG from '$lib/assets/flaticon/layer.png';
 </script>
 
 {#snippet objective(number, imageUrl, title, description)}
@@ -12,6 +17,24 @@
 			<div class="text-4xl font-thin tabular-nums opacity-30">0{number}</div>
 			<img class="size-28 rounded-box" src={imageUrl} alt="objective: {title}" />
 		</div>
+		<div class="flex flex-col gap-2">
+			<div class="text-2xl font-bold">{title}</div>
+			<div class="text-md opacity-90">
+				{description}
+			</div>
+		</div>
+	</li>
+{/snippet}
+
+{#snippet methodology(imageUrl, title, description)}
+	<li class="items-left flex max-w-xs flex-col p-8">
+		<img class="mx-auto size-32 rounded-box" src={imageUrl} alt="objective: {title}" />
+		<svg width="100%" height="40" xmlns="http://www.w3.org/2000/svg" class="mt-8">
+			<!-- Circle 1 -->
+			<circle cx="5%" cy="25" r="10" fill="black" />
+			<!-- Line 1 -->
+			<line x1="5%" y1="25" x2="100%" y2="25" stroke="black" stroke-width="1" />
+		</svg>
 		<div class="flex flex-col gap-2">
 			<div class="text-2xl font-bold">{title}</div>
 			<div class="text-md opacity-90">
@@ -44,7 +67,7 @@
 			</div>
 		</div>
 	</section>
-	<section class="min-h-screen gap-12 bg-zinc-100" title="about">
+	<section class="min-h-screen gap-12 bg-zinc-100" title="objectives">
 		<div class="items-left justify-left space-y-8 p-4 text-left sm:p-2 md:space-y-12 md:p-8">
 			<h1
 				class="max-w-fit bg-[url('custom/yellow-splash-art.png')] bg-cover bg-center p-4 text-3xl font-extrabold text-zinc-800"
@@ -52,7 +75,7 @@
 				Objectives
 			</h1>
 			<ul
-				class="mx-auto flex w-fit justify-center gap-8 overflow-x-auto rounded-box bg-base-100 shadow-md"
+				class="mx-auto flex w-fit justify-center gap-8 overflow-x-auto rounded-box bg-base-100 p-4 shadow-md"
 			>
 				{@render objective(
 					1,
@@ -72,6 +95,19 @@
 					'Evaluate model performance and robustness.',
 					'Measure performance with metrics and run ablation studies to compare GNN configurations.'
 				)}
+			</ul>
+		</div>
+	</section>
+	<section class="min-h-screen gap-12 bg-amber-300" title="methodology">
+		<div class="items-left justify-left space-y-8 p-4 text-left sm:p-2 md:space-y-12 md:p-8">
+			<h1 class="mx-auto max-w-fit p-4 text-3xl font-extrabold text-zinc-800">Methodology</h1>
+			<ul
+				class="mx-auto flex w-fit flex-col justify-center gap-8 overflow-x-auto rounded-box bg-transparent md:flex-row"
+			>
+				{@render methodology(databaseIMG, 'Data Collection', 'insert text here')}
+				{@render methodology(cogsIMG, 'Data Preprocessing', 'insert text here')}
+				{@render methodology(layerIMG, 'BERT-GNN', 'insert text here')}
+				{@render methodology(analysisIMG, 'Evaluation & Deployment', 'insert text here')}
 			</ul>
 		</div>
 	</section>
