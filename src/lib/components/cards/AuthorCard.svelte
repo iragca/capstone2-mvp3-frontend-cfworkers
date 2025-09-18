b798305d1d7478d5e4bcf8e142107f8808390eff
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-09-18 22:27:45 +0800

    feat: boilerplate authors page

---
+++ src/lib/components/cards/AuthorCard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children, name, badge, description } = $props<{
+		children: Snippet;
+		name: string;
+		badge: string;
+		description: string;
+	}>();
+</script>
+
+<div class="card w-96 bg-base-100 shadow-sm">
+	<figure>
+		<img
+			src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+			alt="Shoes"
+		/>
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">
+			{name}
+			{#if badge}
+				<div class="badge badge-primary">{badge}</div>
+			{/if}
+		</h2>
+		<p>
+			{description}
+		</p>
+		<div class="card-actions justify-end">
+			{@render children?.()}
+		</div>
+	</div>
+</div>
---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import GithubBtn from '$lib/components/buttons/GithubBtn.svelte';
+	import LinkedInBtn from '$lib/components/buttons/LinkedInBtn.svelte';
+	import AuthorCard from '$lib/components/cards/AuthorCard.svelte';
+
+	let authors = [
+		{
+			name: 'Karylle Dela Cruz',
+			badge: 'First Author',
+			description:
+				'John is the lead developer of the TRACE project, specializing in backend development and data analysis.'
+		},
+		{
+			name: 'Chris Andrei Irag',
+			badge: 'Lead Developer',
+			description:
+				'John is the lead developer of the TRACE project, specializing in backend development and data analysis.'
+		},
+		{
+			name: 'Karylle Dela Cruz',
+			badge: 'First Author',
+			description:
+				'John is the lead developer of the TRACE project, specializing in backend development and data analysis.'
+		},
+		{
+			name: 'Chris Andrei Irag',
+			badge: 'Lead Developer',
+			description:
+				'John is the lead developer of the TRACE project, specializing in backend development and data analysis.'
+		}
+	];
+</script>
+
+<main class="bg-zinc-100 text-zinc-800">
+	<section
+		class="flex min-h-screen flex-1 flex-col items-center justify-center gap-8 p-4 md:flex-row md:p-12"
+	>
+		<div class="h-12 md:h-0"></div>
+
+		{#each authors as author}
+			<AuthorCard name={author.name} badge={author.badge} description={author.description}>
+				<LinkedInBtn url="https://linkedin.com" />
+				<GithubBtn url="https://github.com" />
+			</AuthorCard>
+		{/each}
+	</section>
+</main>
